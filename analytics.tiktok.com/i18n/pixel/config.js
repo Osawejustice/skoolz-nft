37a2e0d1e3c53a53c6beafffbb27d2176083f9b4
Justice Osawe <osawejustice@gmail.com>
2022-03-17 18:50:11 +0100

    site one

---
+++ analytics.tiktok.com/i18n/pixel/config.js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C7VFJM7V9S6QTNQP8NLG").setAdvancedMatchingAvailableProperties({"email":false,"phone_number":false,"auto_email":false,"auto_phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C7VFJM7V9S6QTNQP8NLG").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C7VFJM7V9S6QTNQP8NLG").setPixelInfo({status: 0, name: "SkullToons", advertiserID: "7061337029384192001", setupMode: 0, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("skulltoons.io");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(false);
